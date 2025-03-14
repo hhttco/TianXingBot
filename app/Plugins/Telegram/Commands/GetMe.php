2e3f676da58a59d9fb390a8d9d6db84cd71d2f11
hhttco <94968015+hhttco@users.noreply.github.com>
2025-03-14 15:30:57 +0800

    Create GetMe.php

---
+++ app/Plugins/Telegram/Commands/GetMe.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Plugins\Telegram\Commands;
+
+use App\Plugins\Telegram\Telegram;
+
+class GetMe extends Telegram {
+    public $command = '/getme';
+    public $description = '获取自己的信息';
+
+    public function handle($message, $match = []) {
+        $telegramService = $this->telegramService;
+        $user = Users::where('telegram_id', $message->user_id)->first();
+        if (!$user) {
+            // abort(500, '用户不存在');
+            $user = new Users;
+            $user->email = $msg->user_id . '@gmail.com';
+            $user->name = $msg->user_name;
+            $user->telegram_id = $msg->user_id;
+            $user->password = $msg->user_id . '@gmail.com';
+
+            $user->save();
+        }
+
+        $userInfo = [
+            '系统ID: ' . $user->id,
+            '用户ID: ' . $msg->user_id,
+            '用户姓名: ' . $msg->user_name,
+        ];
+
+        $text = implode(PHP_EOL, $userInfo);
+        $telegramService->sendMessage($message->chat_id, "当前用户信息：\n\n$text", 'markdown');
+    }
+}
