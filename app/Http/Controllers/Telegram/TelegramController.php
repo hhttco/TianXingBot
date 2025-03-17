ff3ec3d9dee8db89dcd3be45a3f6cf78c21d27bb
hhttco <94968015+hhttco@users.noreply.github.com>
2025-03-17 16:52:14 +0800

    Update TelegramController.php

--- app/Http/Controllers/Telegram/TelegramController.php
+++ app/Http/Controllers/Telegram/TelegramController.php
@@ -20,7 +20,7 @@ class TelegramController extends Controller
         }
 
         $this->telegramService = new TelegramService();
-        $this->thisBotId = '7309768988';
+        $this->thisBotId = explode(':', config('telegram.bot.token'))[0];
     }
 
     public function webhook(Request $request)
@@ -148,24 +148,10 @@ class TelegramController extends Controller
 
     public function newJoinMember(array $data)
     {
-        // 如果不是新加入
-        if (!isset($data['message']['new_chat_participant'])) return;
+        $class = '\\App\\Plugins\\Telegram\\Commands\\NewJoinMember';
+        if (!class_exists($class)) return;
+        $instance = new $class();
 
-        $chatId = $data['message']['chat']['id'];
-        $newMemberId = $data['message']['new_chat_participant']['id'];
-
-        // 获取用户姓名
-        $userName = $data['message']['new_chat_participant']['first_name'];
-        if (isset($data['message']['new_chat_participant']['last_name'])) {
-            $userName = $userName . " " . $data['message']['new_chat_participant']['last_name'];
-        }
-
-        if ($newMemberId == $this->thisBotId) {
-            $retText = "[$userName](tg://user?id=$newMemberId) 请将本机器人设置为管理员";
-        } else {
-            $retText = "欢迎新用户 [$userName](tg://user?id=$newMemberId)";
-        }
-
-        $this->telegramService->sendMessage($chatId, $retText, 'markdown');
+        $instance->handle($data);
     }
 }
