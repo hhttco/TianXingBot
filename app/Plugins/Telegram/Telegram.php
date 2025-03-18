bd4eb813c1c9326dccaf25404af36b1adc72d222
hhttco <94968015+hhttco@users.noreply.github.com>
2025-03-18 17:34:17 +0800

    Update Telegram.php

--- app/Plugins/Telegram/Telegram.php
+++ app/Plugins/Telegram/Telegram.php
@@ -7,9 +7,11 @@ use App\Services\TelegramService;
 abstract class Telegram {
     abstract protected function handle($message, $match);
     public $telegramService;
+    public $common;
 
     public function __construct()
     {
         $this->telegramService = new TelegramService();
+        $this->common = new Common();
     }
 }
