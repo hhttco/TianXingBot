0dd624111228ad5d0edb8982b311f6b34334c8ae
hhttco <94968015+hhttco@users.noreply.github.com>
2025-03-18 17:37:34 +0800

    Create 2025_03_18_083901_create_tg_group_config_table.php

---
+++ database/migrations/2025_03_18_083901_create_tg_group_config_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTgGroupConfigTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tg_group_config', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('group_id', 100);
+            $table->longText('group_welcome'); // 群欢迎
+            $table->tinyInteger('group_welcome_state')->default(0); // 默认 0 关闭 1 启用
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tg_group_config');
+    }
+}
