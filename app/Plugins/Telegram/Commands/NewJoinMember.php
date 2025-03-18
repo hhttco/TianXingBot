a49949f617918c1e4433ef006afc35dd4a7d8447
hhttco <94968015+hhttco@users.noreply.github.com>
2025-03-18 15:14:00 +0800

    Update NewJoinMember.php

--- app/Plugins/Telegram/Commands/NewJoinMember.php
+++ app/Plugins/Telegram/Commands/NewJoinMember.php
@@ -6,6 +6,15 @@ use App\Services\TelegramService;
 use App\Models\TgGroup;
 
 class NewJoinMember {
+    protected $telegramService;
+    protected $thisBotId;
+
+    public function __construct()
+    {
+        $this->telegramService = new TelegramService();
+        $this->thisBotId = explode(':', config('telegram.bot.token'))[0];
+    }
+
     public function handle($data) {
         // 如果是离开群组
         if (isset($data['message']['left_chat_participant'])) {
@@ -25,7 +34,7 @@ class NewJoinMember {
             $userName = $userName . " " . $data['message']['new_chat_participant']['last_name'];
         }
 
-        if ($newMemberId == explode(':', config('telegram.bot.token'))[0]) {
+        if ($newMemberId == $this->thisBotId) {
             $retText = "[$userName](tg://user?id=$newMemberId) 请将本机器人设置为管理员";
 
             // 如果是被邀请进入判断数据库是否存在
@@ -34,14 +43,13 @@ class NewJoinMember {
             $retText = "欢迎新用户 [$userName](tg://user?id=$newMemberId)";
         }
 
-        $telegramService = new TelegramService();
-        $telegramService->sendMessage($chatId, $retText, 'markdown');
+        $this->telegramService->sendMessage($chatId, $retText, 'markdown');
     }
 
     public function left($data) {
         $groupId = $data['message']['chat']['id'];
         $leftMemberId = $data['message']['left_chat_participant']['id'];
-        if ($leftMemberId == explode(':', config('telegram.bot.token'))[0]) {
+        if ($leftMemberId == $this->thisBotId) {
             // 更新数据库状态
             $group = TgGroup::where('group_id', $groupId)->first();
             if ($group) {
@@ -71,8 +79,11 @@ class NewJoinMember {
             $group->group_id = $groupId;
         }
 
+        // 当前群组人数
+        $n = $this->telegramService->getChatMemberCount($groupId);
+
         $group->group_name = $data['chat']['title'];
-        $group->group_user_num = 0;
+        $group->group_user_num = $n->result;
         $group->create_user_id = $data['from']['id'];
         $group->create_user_name = $userName;
         $group->group_bot_state = 1;
