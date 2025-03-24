468d1c96f443bd6cb9d42fb8b79b45829eed1d6b
hhttco <94968015+hhttco@users.noreply.github.com>
2025-03-24 18:43:12 +0800

    Create Help.php

---
+++ app/Plugins/Telegram/Commands/Help.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Plugins\Telegram\Commands;
+
+use App\Plugins\Telegram\Telegram;
+
+class Help extends Telegram {
+    public $command = '/help';
+    public $description = '获取帮助信息';
+
+    public function handle($message, $match = []) {
+        if (!$message->is_private) return;
+
+        $this->common->help($message->chat_id);
+    }
+}
