f1b8d6db5fc0d654401a9a2c107cf88f133fc3eb
hhttco <94968015+hhttco@users.noreply.github.com>
2025-03-28 16:18:24 +0800

    Update Ckans.php

--- app/Plugins/Telegram/Commands/Ckans.php
+++ app/Plugins/Telegram/Commands/Ckans.php
@@ -3,25 +3,46 @@
 namespace App\Plugins\Telegram\Commands;
 
 use App\Plugins\Telegram\Telegram;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redis;
+use App\Models\TgGroupConfig;
 
 class Ckans extends Telegram {
     public $command = '/ckans';
     public $description = '入群验证回答';
 
     public function handle($message, $match = []) {
-        $this->common->help($message->chat_id);
+        // $this->common->help($message->chat_id);
+        $redisKey = 'checkJoin' . $message->chat_id . $message->user_id;
+        if (!Redis::exists($redisKey)) return;
 
-        Log::info('获取的答案是=>>>>>' . $message->check_answer);
-        Log::info('当前时间戳=>>>>>' . time());
-        Log::info('当前时间戳加上90=>>>>>' . (time() + 90));
+        $redisAns = Redis::get($redisKey);
+        if ($redisAns == $message->check_answer) {
+            // 回答正确
 
-        Log::info('数据是' . $message->chat_id . ' -- ' . $message->user_id);
+            // 回答正确后解除限制
+            $this->telegramService->restrictChatMember($message->chat_id, $message->user_id, 1, true);
 
-        // 回答正确后解除限制
-        $this->telegramService->restrictChatMember($message->chat_id, $message->user_id, 1, true);
+            // 删除
+            $this->telegramService->deleteMessage($message->chat_id, $message->message_id);
 
-        // 删除
-        $this->telegramService->deleteMessage($message->chat_id, $message->message_id);
+            // 发送欢迎词
+            $groupConfig = TgGroupConfig::where('group_id', $message->chat_id)->first();
+            if ($groupConfig) {
+                if ($groupConfig->group_welcome_state == 1 && $groupConfig->group_welcome) {
+                    $this->common->welcome($message->chat_id, $message->user_id, $message->user_name, $groupConfig->group_welcome);
+                    return;
+                }
+
+                $retText = "欢迎新用户 [$message->user_name](tg://user?id=$message->user_id)";
+                $this->telegramService->sendMessage($message->chat_id, $retText, 'markdown');
+            }
+        } else {
+            // 回答错误
+
+            // 踢出群组
+            $this->telegramService->banChatMember($message->chat_id, $message->user_id, time() + 90);
+        }
+
+        Redis::del($redisKey);
     }
 }
