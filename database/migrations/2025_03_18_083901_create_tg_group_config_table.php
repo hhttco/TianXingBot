2abc033e2a821c69a4e04724d5710508008aa57c
hhttco <94968015+hhttco@users.noreply.github.com>
2025-03-28 16:23:30 +0800

    Update 2025_03_18_083901_create_tg_group_config_table.php

--- database/migrations/2025_03_18_083901_create_tg_group_config_table.php
+++ database/migrations/2025_03_18_083901_create_tg_group_config_table.php
@@ -18,6 +18,7 @@ class CreateTgGroupConfigTable extends Migration
             $table->string('group_id', 100);
             $table->longText('group_welcome'); // 群欢迎
             $table->tinyInteger('group_welcome_state')->default(0); // 默认 0 关闭 1 启用
+            $table->tinyInteger('group_join_check')->default(0); // 入群验证 默认 0 关闭 1 启用
             $table->timestamps();
         });
     }
