523717325f017e939e6c757e981d11d5a4350252
hhttco <94968015+hhttco@users.noreply.github.com>
2025-03-28 16:25:26 +0800

    Create ConfigWelcome.php

---
+++ app/Plugins/Telegram/Commands/ConfigWelcome.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Plugins\Telegram\Commands;
+
+use App\Plugins\Telegram\Telegram;
+
+class ConfigWelcome extends Telegram {
+    public $command = '/configwelcome';
+    public $description = '关闭欢迎语';
+
+    public function handle($message, $match = []) {
+        if ($message->is_private) {
+            abort(500, '请在群聊中设置');
+        };
+
+        $this->common->editConfig($message->chat_id, $message->message_id, $message->user_id, 'configwelcome');
+    }
+}
