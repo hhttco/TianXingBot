5aedbc28b136ebd93ef7b301d9f8483988d41f26
hhttco <94968015+hhttco@users.noreply.github.com>
2025-03-28 16:20:30 +0800

    Update Common.php

--- app/Plugins/Telegram/Common.php
+++ app/Plugins/Telegram/Common.php
@@ -3,6 +3,8 @@
 namespace App\Plugins\Telegram;
 
 use App\Services\TelegramService;
+use Illuminate\Support\Facades\Redis;
+use App\Models\TgGroupConfig;
 
 class Common {
     protected $telegramService;
@@ -15,9 +17,9 @@ class Common {
         $this->thisBotId = explode(':', config('telegram.bot.token'))[0];
     }
 
-    public function welcome($str, $userName, $chatId) {
-        // 替换名称
-        $str = str_replace('{$username}', $userName, $str);
+    public function welcome($chatId, $userId, $userName, $str) {
+        // 替换名称 单引号 不支持变量解析 双引号支持变量解析
+        $str = str_replace('{$username}', "[$userName](tg://user?id=$userId)", $str);
 
         // 设置按钮 explode
         $arr = explode('||', $str);
@@ -69,21 +71,103 @@ class Common {
     // 发送入群验证
     public function checkJoin($chatId, $userId, $userName) {
         // 限制聊天 回答正确后解除限制
-        $this->telegramService->restrictChatMember($chatId, $userId, time() + 90, false);
+        $this->telegramService->restrictChatMember($chatId, $userId, time() + 1, false);
 
-        $ques = $userName . " 你好呀！请回答一个问题，7+3等于多少？请在90秒内回答，否则会被我踢出群。";
+        $firstNum  = rand(1, 9);
+        $secondNum = rand(1, 9);
+        $ques = $userName . ' 你好呀！请回答一个问题，' . $firstNum . ' + ' . $secondNum . ' 等于多少？请在90秒内回答，否则会被我踢出群。';
+        $replyMarkup = $this->getCheckJoinStr($chatId, $userId, $firstNum, $secondNum);
 
+        $this->telegramService->sendMessageMarkup($chatId, $ques, $replyMarkup, 'markdown');
+    }
+
+    // 获取入群验证发送键盘的字符串
+    public function getCheckJoinStr($chatId, $userId, $firstNum, $secondNum) {
+        $ansNum = $firstNum + $secondNum;
+
+        $itemArr = array();
+        $itemArr[] = ['text' => $ansNum, 'callback_data' => '/ckans-' . $ansNum];
+        for ($i = 0; $i < 3; $i++) { 
+            $iNum = rand(1, 20);
+            if ($iNum == $ansNum) {
+                $i--;
+                continue;
+            }
+
+            $arrItme = ['text' => $iNum, 'callback_data' => '/ckans-' . $iNum];
+            $itemArr[] = $arrItme;
+        }
+
+        // 重新排列顺序
+        shuffle($itemArr);
+
+        $ret = [
+            'inline_keyboard' => [
+                $itemArr
+            ]
+        ];
+
+        // 把正确答案存到 redis
+        Redis::setex('checkJoin' . $chatId . $userId, 8640, $ansNum);
+
+        $replyMarkup = json_encode($ret);
+
+        return $replyMarkup;
+    }
+
+    // 修改配置
+    public function editConfig($chatId, $messageId, $userId, $type) {
+        // 权限验证
+        if (!$this->power($chatId, $userId)) {
+            abort(500, '请联系管理员操作');
+        }
+
+        // 获取配置列表
+        $groupConfig = TgGroupConfig::where('group_id', $chatId)->first();
+        if (!$groupConfig) {
+            abort(500, '请重新邀请本机器人入群');
+        }
+
+        // 修改数据
+        if ($type === 'configwelcome') {
+            $groupConfig->group_welcome_state = $groupConfig->group_welcome_state ^ 1;
+        }
+
+        if ($type === 'configjoincheck') {
+            $groupConfig->group_join_check = $groupConfig->group_join_check ^ 1;
+        }
+
+        $sendText = '感谢使用本机器人！使用前请将本机器人设置为管理员';
+        $replyMarkup = $this->getChannelConfig($groupConfig);
+
+        // 如果是获取配置
+        if ($type === 'config') {
+            $this->telegramService->sendMessageMarkup($chatId, $sendText, $replyMarkup, 'markdown');
+        } else {
+            $groupConfig->save();
+            $this->telegramService->editMessageMarkup($chatId, $messageId, $sendText, $replyMarkup, 'markdown');
+        }
+    }
+
+    public function getChannelConfig($groupConfig) {
+        // 发送消息
         $replyMarkup = json_encode([
             'inline_keyboard' => [
                 [
-                    ['text' => "8", 'callback_data' => '/ckans-8'],
-                    ['text' => "9", 'callback_data' => '/ckans-9'],
-                    ['text' => "10", 'callback_data' => '/ckans-10'],
-                    ['text' => "14", 'callback_data' => '/ckans-14']
+                    ['text' => '获取配置列表', 'callback_data' => '/config']
+                ],
+                [
+                    ['text' => $groupConfig->group_welcome_state ? '✅ 关闭欢迎词' : '❌ 开启欢迎词', 'callback_data' => '/configwelcome']
+                ],
+                [
+                    ['text' => $groupConfig->group_join_check ? '✅ 关闭入群验证' : '❌ 开启入群验证', 'callback_data' => '/configjoincheck']
+                ],
+                [
+                    ['text' => '关闭配置菜单', 'callback_data' => '/configclose']
                 ]
             ]
         ]);
 
-        $this->telegramService->sendMessageMarkup($chatId, $ques, $replyMarkup, 'markdown');
+        return $replyMarkup;
     }
 }
