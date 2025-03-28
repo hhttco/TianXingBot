d6458dbb97f82abdf506b50315256554ad19084f
hhttco <94968015+hhttco@users.noreply.github.com>
2025-03-28 16:21:05 +0800

    Update NewJoinMember.php

--- app/Plugins/Telegram/NewJoinMember.php
+++ app/Plugins/Telegram/NewJoinMember.php
@@ -9,11 +9,13 @@ use App\Models\TgGroupConfig;
 class NewJoinMember {
     protected $telegramService;
     protected $thisBotId;
+    protected $common;
 
     public function __construct()
     {
         $this->telegramService = new TelegramService();
         $this->thisBotId = explode(':', config('telegram.bot.token'))[0];
+        $this->common = new Common();
     }
 
     public function handle($data) {
@@ -36,29 +38,33 @@ class NewJoinMember {
         }
 
         if ($newMemberId == $this->thisBotId) {
-            $retText = "[$userName](tg://user?id=$newMemberId) 请将本机器人设置为管理员";
-
             // 如果是被邀请进入判断数据库是否存在
             $this->group($data);
         } else {
             // 判断是否有配置
             $groupConfig = TgGroupConfig::where('group_id', $chatId)->first();
-            if ($groupConfig && $groupConfig->group_welcome_state == 1) {
-                $common = new Common();
-                $common->welcome($groupConfig->group_welcome, $userName, $chatId);
-                return;
-            } else {
-                // 如果是创建者禁言函数无法返回结果
-                $common = new Common();
-                if (!$common->power($chatId, $newMemberId)) {
-                    $common->checkJoin($chatId, $newMemberId, $userName);
-                }
-
-                $retText = "欢迎新用户 [$userName](tg://user?id=$newMemberId)";
+            if (!$groupConfig) {
+                abort(500, '请重新邀请机器人入群');
             }
-        }
 
-        $this->telegramService->sendMessage($chatId, $retText, 'markdown');
+            // 判断是否开启了入群验证
+            if ($groupConfig->group_join_check == 1) {
+                // 如果是创建者禁言函数无法返回结果
+                if (!$this->common->power($chatId, $newMemberId)) {
+                    $this->common->checkJoin($chatId, $newMemberId, $userName);
+                    return;
+                }
+            }
+
+            // 开启了欢迎词并且存在欢迎词
+            if ($groupConfig->group_welcome_state == 1 && $groupConfig->group_welcome) {
+                $this->common->welcome($chatId, $newMemberId, $userName, $groupConfig->group_welcome);
+                return;
+            }
+
+            $retText = "欢迎新用户 [$userName](tg://user?id=$newMemberId)";
+            $this->telegramService->sendMessage($chatId, $retText, 'markdown');
+        }
     }
 
     public function left($data) {
@@ -104,5 +110,18 @@ class NewJoinMember {
         $group->group_bot_state = 1;
 
         $group->save();
+
+        // 群组配置表
+        $groupConfig = TgGroupConfig::where('group_id', $groupId)->first();
+        if (!$groupConfig) {
+            $groupConfig = new TgGroupConfig;
+            $groupConfig->group_id = $groupId;
+            $groupConfig->save();
+        }
+
+        // 发送配置欢迎
+        $sendText = '感谢使用本机器人！使用前请将本机器人设置为管理员';
+        $replyMarkup = $this->common->getChannelConfig($groupConfig);
+        $this->telegramService->sendMessageMarkup($groupId, $sendText, $replyMarkup, 'markdown');
     }
 }
