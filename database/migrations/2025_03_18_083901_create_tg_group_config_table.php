79a777b12ec3a423e1ac698bb4badfc697468324
hhttco <94968015+hhttco@users.noreply.github.com>
2025-04-02 16:23:10 +0800

    Update 2025_03_18_083901_create_tg_group_config_table.php

--- database/migrations/2025_03_18_083901_create_tg_group_config_table.php
+++ database/migrations/2025_03_18_083901_create_tg_group_config_table.php
@@ -16,7 +16,7 @@ class CreateTgGroupConfigTable extends Migration
         Schema::create('tg_group_config', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('group_id', 100);
-            $table->longText('group_welcome'); // 群欢迎
+            $table->longText('group_welcome')->nullable(); // 群欢迎
             $table->tinyInteger('group_welcome_state')->default(0); // 默认 0 关闭 1 启用
             $table->tinyInteger('group_join_check')->default(0); // 入群验证 默认 0 关闭 1 启用
             $table->timestamps();
