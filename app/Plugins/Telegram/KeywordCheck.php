0fba49a27ab9bddcb1d38a1ca5f1187fdd19ba5c
hhttco <94968015+hhttco@users.noreply.github.com>
2026-02-06 15:33:04 +0800

    Update KeywordCheck.php

--- app/Plugins/Telegram/KeywordCheck.php
+++ app/Plugins/Telegram/KeywordCheck.php
@@ -47,13 +47,13 @@ class KeywordCheck {
     }
 
     public function delForwardMsg($data) {
+        Log::info(json_encode($data));
         // 删除转发消息
         if (isset($data['message']['forward_origin']) ||
             isset($data['message']['forward_from_chat']) ||
             isset($data['message']['forward_from_message_id']) ||
             isset($data['message']['forward_date'])) {
             $this->telegramService->deleteMessage($data['message']['chat']['id'], $data['message']['message_id']);
-        $this->telegramService->deleteMessage($data['message']['chat']['id'], $data['message']['forward_from_message_id']);
         }
     }
 }
